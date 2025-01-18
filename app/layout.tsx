1ac96abd154ba14aa351300d0c730bf4b3695861
Mutlaq Aldhbuiub <matulkn@gmail.com>
2025-01-18 14:36:54 +0300

    Fixing building issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,12 @@ import Footer from '@/components/Footer'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
-const almarai = Almarai({ subsets: ['arabic'], weight: ['300', '400', '700', '800'] })
+const almarai = Almarai({
+  subsets: ['arabic'],
+  weight: ['300', '400', '700'],
+  variable: '--font-almarai',
+  preload: true,
+})
 
 export const metadata: Metadata = {
   title: 'Zahma O La - Your Fastest Destination for King Fahd Causeway Traffic',
@@ -20,7 +25,7 @@ export default function RootLayout({
 }) {
   return (
     <html suppressHydrationWarning>
-      <body className={`${inter.className} ${almarai.variable}`}>
+      <body className={`${inter.className} ${almarai.className}`}>
         <Providers>
           <Header />
           <main>{children}</main>
